549f3de3d76407a768bbe29f74b664d4bc743938
Gor Martsen <Gor.martsen@gmail.com>
2016-06-04 15:36:10 -0400

    Create install file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Go to domain directory.
+cd $DOCROOT
+
+# Link Backdrop files
+ln -s $ZENCI_DEPLOY_DIR/* ./
+ln -s $ZENC_DEPLOY_DIR/.htaccess ./
+
+# Unlink settings.php and copy instead.
+rm -f settings.php
+cp $ZENCI_DEPLOY_DIR/settings.php ./
+
+# Unlink files and copy instead.
+rm -f files
+cp -r $ZENCI_DEPLOY_DIR/files ./
+
+# Unlink sites and copy instead.
+rm -f sites
+cp -r $ZENCI_DEPLOY_DIR/sites ./
+
+
+
+# Install Backdrop.
+php $DOCROOT/core/scripts/install.sh  --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME --root=$DOCROOT
